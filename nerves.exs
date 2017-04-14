46da59313d88da998c7a44fd74aed6bcac0372e8
Klaus Alfert <klaus.alfert@gmail.com>
2017-04-14 21:40:04 +0200

    Use the checksums again. Otherwise another build is not started independently if the this file change.
    But, alas, a new container is generated it takes quite long to build that stuff.

--- nerves.exs
+++ nerves.exs
@@ -11,9 +11,9 @@ config pkg, :nerves_env,
   type: :system,
   version: version,
   compiler: :nerves_package,
-  artifact_url: [
-    "https://github.com/tmecklem/#{pkg}/releases/download/v#{version}/#{pkg}-v#{version}.tar.gz",
-  ],
+  # artifact_url: [
+  #   "https://github.com/tmecklem/#{pkg}/releases/download/v#{version}/#{pkg}-v#{version}.tar.gz",
+  # ],
   platform: Nerves.System.BR,
   platform_config: [
     defconfig: "nerves_defconfig"
@@ -21,12 +21,12 @@ config pkg, :nerves_env,
   checksum: [
     # commented out during compile/test to prevent creating new docker images
     # after every change.
-    # "nerves_defconfig",
-    # "rootfs-additions",
-    # "linux-4.4.defconfig",
-    # "fwup.conf",
-    # "cmdline.txt",
-    # "config.txt",
-    # "post-createfs.sh",
-    # "VERSION"
+    "nerves_defconfig",
+    "rootfs-additions",
+    "linux-4.4.defconfig",
+    "fwup.conf",
+    "cmdline.txt",
+    "config.txt",
+    "post-createfs.sh",
+    "VERSION"
   ]
